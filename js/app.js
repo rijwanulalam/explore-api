59a59736b438755e3d7601b389b565ae47c68170
rijwanulalam <rijwanul007@gmail.com>
2023-11-20 13:24:02 +0600

    country done

--- js/app.js
+++ js/app.js
@@ -13,17 +13,37 @@ const getCountryDetails = countries => {
         postDiv.classList.add("border", "p-5", "rounded-lg")
         postDiv.innerHTML = `
             <img class="h-48" src="${country.flags.svg}" alt="${country.flags.alt}" >
-            <h3 class="font-bold my-5">Name : ${country.name.official}</h3>
+            <h3 class="font-bold my-5">Name : ${country.name.common}</h3>
             <h5>Capital : ${country.capital ?country.capital[0] : "no-capital"}</h5>
             <p class="my-5">Population : ${country.population}</p>
-            <button onclick="loadContryDetails()" class="bg-sky-200 font-medium px-5 py-2 rounded-lg hover:bg-sky-300">Details</button>
+            <button onclick="loadContryDetails('${country.cca2}')" class="bg-sky-200 font-medium px-5 py-2 rounded-lg hover:bg-sky-300">Details</button>
         `
         section.appendChild(postDiv);
     });
 }
 
-function loadContryDetails(){
-    console.log("clicked")
+const loadContryDetails = code => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountryDetails(data))
+}
+
+const showCountryDetails = country => {
+    // console.log(country)
+    const detailsContainer = document.getElementById("country-details");
+    const showDiv = document.createElement("div");
+    showDiv.innerHTML = `
+    <img src= "${country[0].flags.png}" >
+    <p class="my-2">Official Name : <span class="font-medium">${country[0].name.official}</span></p>
+    
+    <p class="my-2">Continents : <span class="font-medium">${country[0].continents[0]}</span></p>
+    <p class="my-2">Continents : <span class="font-medium">${country[0].region}</span></p>
+    <p class="my-2">Subregion : <span class="font-medium">${country[0].subregion}</span></p>
+    
+
+    `
+    detailsContainer.appendChild(showDiv);
 }
 
 loadContriesInfo();
